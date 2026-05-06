123d274e4bb0619f7ebf2cb9cbc839e877381f08
mrschljakhta-max <mr.schljakhta@gmail.com>
2026-05-06 21:34:21 +0300

    Create schema.js

---
+++ scripts/schema.js
@@ -0,0 +1,16 @@
+const ART_AMMO_SCHEMA = {
+  SUMMARY_SHEET_NAME: "АГ3+АР",
+
+  START_ROW_INDEX: 5,
+
+  COLUMNS: {
+    category: 0,
+    ammo: 2,
+    rangeMeters: 3,
+    received: 4,
+    spent: 5,
+    balance: 6
+  },
+
+  LONG_RANGE_KM: 18
+};
