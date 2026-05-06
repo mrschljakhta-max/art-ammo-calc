d253b884b5d912ae90e6edd61922c6a9daf87d99
mrschljakhta-max <mr.schljakhta@gmail.com>
2026-05-06 18:57:01 +0300

    Update excel.js

--- scripts/excel.js
+++ scripts/excel.js
@@ -1,1 +1,54 @@
+const excelInput = document.getElementById("excelFile");
 
+const fileInfo = document.getElementById("fileInfo");
+
+const sheetList = document.getElementById("sheetList");
+
+excelInput.addEventListener("change", handleExcel);
+
+function handleExcel(event) {
+
+  const file = event.target.files[0];
+
+  if (!file) return;
+
+  fileInfo.innerHTML = `
+    <p>Файл: ${file.name}</p>
+  `;
+
+  const reader = new FileReader();
+
+  reader.onload = function(e) {
+
+    const data = new Uint8Array(e.target.result);
+
+    const workbook = XLSX.read(data, {
+      type: "array"
+    });
+
+    renderSheets(workbook);
+
+  };
+
+  reader.readAsArrayBuffer(file);
+}
+
+function renderSheets(workbook) {
+
+  sheetList.innerHTML = "";
+
+  workbook.SheetNames.forEach(sheetName => {
+
+    const div = document.createElement("div");
+
+    div.className = "sheet-item";
+
+    div.innerHTML = `
+      ${sheetName}
+    `;
+
+    sheetList.appendChild(div);
+
+  });
+
+}
